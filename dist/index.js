807be3023625ac6656b621c0877e4868be9a1438
stepbystep <likerberi97@gmail.com>
2018-07-17 17:34:21 +0900

    skeleton added

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "이병용";
+const age = "31";
+const gender = "male";
+const printFunc = (name, age, gender) => {
+    console.log('Hello ${이병용}, ${age}_${gender}!');
+};
+printFunc("testName", age, gender);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+
+const name="이병용";
+const age="31";
+const gender="male";
+
+const printFunc = (name, age, gender) => {
+	console.log('Hello ${이병용}, ${age}_${gender}!');
+}
+
+printFunc("testName", age, gender);
+
+export {};
