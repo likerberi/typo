b8279e87aefa1ddb84c2b3b5eec3e26ebda7e81a
stepbystep <likerberi97@gmail.com>
2018-07-18 15:55:34 +0900

    part 1

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
 	public index: number;
 	public hash : string;
 	public previousHash: string;
 	public data : string;
 	public timestamp: number;
+
+	static calculateBlockHash = (
+		index: number, 
+		previousHash: string,
+		timestamp: number,
+		data: string
+	): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
 	constructor(
 		index: number,
 		hash : string,
 		previousHash: string,
 		data : string,
 		timestamp: number
-	){
+	) {
 		this.index = index;
 		this.hash  = hash;
 		this.previousHash = previousHash;
@@ -19,8 +29,20 @@ class Block {
 	}
 }
 
+
 const genesisBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
 console.log(blockchain);
+console.log(getBlockchain());
+console.log(getLatestBlock());
+console.log(getNewTimeStamp());
+
 export {};
