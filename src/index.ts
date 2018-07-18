4d28d5bdec48d3d38976a605c4515575725e73bc
stepbystep <likerberi97@gmail.com>
2018-07-18 17:24:59 +0900

    simply done

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,13 @@ class Block {
 		data: string
 	): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+	static validateStructure = (aBlock : Block) : boolean => 
+		typeof aBlock.index === "number" &&
+		typeof aBlock.hash === "string" &&
+		typeof aBlock.previousHash === "string" && 
+		typeof aBlock.timestamp === "number" &&
+		typeof aBlock.data === "string";
+
 	constructor(
 		index: number,
 		hash : string,
@@ -40,9 +47,56 @@ const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data:string) : Block => {
+	const previousBlock: Block = getLatestBlock();
+	const newIndex : number = previousBlock.index + 1;
+	const newTimeStamp : number = getNewTimeStamp();
+	const newHash : string = Block.calculateBlockHash(
+		newIndex, 
+		previousBlock.hash, 
+		newTimeStamp, 
+		data, 
+	);
+	const newBlock : Block = new Block(
+		newIndex, 
+		newHash, 
+		previousBlock.hash, 
+		data, 
+		newTimeStamp
+	);
+	addBlock(newBlock);
+	return newBlock;
+};
+
+const getHashforBlock = (aBlock : Block) : string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (
+	candidateBlock : Block,
+	previousBlock : Block
+) : boolean => {
+	if(!Block.validateStructure(candidateBlock)) {
+		return false;
+	} else if (previousBlock.index + 1 !== candidateBlock.index) {
+		return false;
+	} else if (previousBlock.hash !== candidateBlock.previousHash) {
+		return false;
+	} else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+		return false;
+	} else {
+		return true;
+	}
+};
+
+const addBlock = (candidateBlock : Block) : void => {
+	if (isBlockValid(candidateBlock, getLatestBlock())) {
+		blockchain.push(candidateBlock);
+	}
+};
+
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+
 console.log(blockchain);
-console.log(getBlockchain());
-console.log(getLatestBlock());
-console.log(getNewTimeStamp());
 
 export {};
